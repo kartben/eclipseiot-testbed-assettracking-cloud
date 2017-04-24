074aa5b2881db4b9b730bdf2c680088d9fa23fde
Benjamin Cabé <benjamin@eclipse.org>
2017-04-24 11:57:16 +0200

    remove fleet telematics from Eclipse testbed

--- dashboard/app/routes.js
+++ dashboard/app/routes.js
@@ -4,9 +4,6 @@ angular.module('app').config([ '$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl : 'partials/home.html',
     controller : 'HomeController'
-  }).when('/exec', {
-      templateUrl : 'partials/exec/home.html',
-      controller : 'ExecHomeController'
   }).otherwise({
     redirectTo : '/'
   });
